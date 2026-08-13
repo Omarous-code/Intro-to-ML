cb213f09763f3dc875f47f161f9bb1d323e4b8bf
Omar <kaffelomar06@gmail.com>
2026-08-13 15:24:00 +0100

    OMG THIS WORKS :3

--- STRING REPRODUCTION.py
+++ STRING REPRODUCTION.py
@@ -7,17 +7,12 @@ def random_string(length):
     return ''.join(choice(letters) for i in range(length))
 target_string = input("Enter a string: ")
 len_string = len(target_string)
-scores = []
 pop = [random_string(len_string) for j in range (20)]
 found_sol = False
-sortarr = []
-children = []
 
-def v_tuple(tuple):
-    for item in tuple:
-        print(item)
 
 def evaluate(population):
+    scores = []
     for string in population:
         score = 0
         for i in range(len_string):
@@ -32,31 +27,51 @@ def sort(scores):
     sorted_scores = sorted(scores, key=lambda x: x[1], reverse=True)
     return sorted_scores
 def mate(sorted_scores):
+    sortarr = []
+    children = []
     for item in sorted_scores:
         sortarr.append(item[0])
     for i in range(20):
         child = ""
         Hindividual = sortarr[randint(0, 4)]
-        Lindividual = sortarr[randint(15, 19)]
+        Lindividual = sortarr[randint(10, 15)]
         for f in range(len_string):
             child += choice(Hindividual[f] + Lindividual[f])
         children.append(child)
     return children
 
 
-print(sort(evaluate(pop)))
-print(mate(sort(evaluate(pop))))
+
+
 
 def mutate(population):
     for i in range(20):
-        if randint(0, 100) >= 90:
+        if randint(0, 100) > 90:
             listed_string = list(population[i])
             listed_string[randint(0, len_string -1)] = choice(letters)
             population[i] = ''.join(listed_string)
     return population
 
+def check_score(sorted_scores):
+    global found_sol
+    if sorted_scores[0][1] == 100:
+        found_sol = True
 
 
-print(mutate(mate(sort(evaluate(pop)))))
 
-print(target_string)
+
+
+while found_sol == False:
+    sorted_population = (sort(evaluate(pop)))
+    check_score(sorted_population)
+    print(sorted_population)
+    mated_population = mate(sorted_population)
+    print(mated_population)
+    mutated_population = mutate(mated_population)
+    print(mutated_population)
+    pop.clear()
+    pop = mutated_population
+    print(pop)
+    print("this is pop")
+    print(target_string)
+
